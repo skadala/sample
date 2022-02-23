51ebd5830982121fc9d204aeee10886d3ec69fe7
skadala <91518705+skadala@users.noreply.github.com>
2022-02-23 23:54:15 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -13,61 +13,27 @@ export default class App extends React.Component {
       s.async = true;
       // s.innerHTML = "adserver.display('Test_ATF');";
       this.instance.appendChild(s)
-    if ("OTPCredential" in window) {
-      console.log("supported webotp")
-      const ac = new AbortController();
-
-      navigator.credentials
-        .get({
-          otp: { transport: ["sms"] },
-          signal: ac.signal,
-        })
-        .then((otp) => {
-          this.setState({ otp: otp.code });
-          ac.abort();
-        })
-        .catch((err) => {
-          ac.abort();
-          console.log(err);
-        });
-    }
-    else{
-      console.log("no support for webotp")
-    }
+    const s1 = document.createElement('script');
+      s1.type = 'text/javascript';
+//       s1.id="AV62063c7d977bcd0ce57a7ac9"
+//       s1.src='https://tg1.aniview.com/api/adserver/spt?AV_TAGID=62063c7d977bcd0ce57a7ac9&AV_PUBLISHERID=5fe07fe623406812b82d078c'
+      s1.async = false;
+      // s1.innerHTML = "adserver.display('Test_ATF');";
+      this.instance.appendChild(s1)
+    
   }
 
   render() {
     return (
       <div className="App">
-        <br />
-        <br />
-        <br />
-        <br />
-        <h2>Your OTP is: {this.state.otp}</h2>
-        <br />
-        <br />
-        <br />
-        <br />
-        <h3>The Web OTP API Docs</h3>
+        
         <div>
           <div id='Test_ATF' ><script>adserver.display('Test_ATF');</script></div>
             
-          Send an SMS that includes
+          
           <div id='Test_ATF' ref={el => (this.instance = el)} style={{height:"100"}}/> 
-          <pre>
-            <code>@preprodportal.vodafoneplay.in #12345</code>
-          </pre>
-          at the last line to this phone.
-          <pre>
-            <code>please don't use https:// and / end of the url</code>
-          </pre>
-          <pre style={{ color: "green" }}>
-            <code>example for use @www.google.com</code>
-          </pre>
-          <pre style={{ color: "red" }}>
-            <code>example for not use XXXXX @https://www.google.com XXXXX</code>
-          </pre>
-<script id="ionAdTagScript" src="//cdn.torcai.com/tags/js/ionadtag.js?dm=satishkadala.netlify.app&pageUrl=satishkadala.netlify.app&adUnit=Test_ATF" async></script>
+          
+<script id="ionAdTagScript" src="//cdn.torcai.com/tags/js/ionadtag.js?dm=https://satishkadala.netlify.app&pageUrl=https://satishkadala.netlify.app&adUnit=Test_ATF" async></script>
         </div>
       </div>
     );
